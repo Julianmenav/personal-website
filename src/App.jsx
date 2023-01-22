e840a6a2be5ad2584dd0c2e0147a7888ee85e610
juli <julianmenav@gmail.com>
2023-01-22 18:03:58 +0100

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App text-red-200">
+      Hello World v2
+    </div>
+  )
+}
+
+export default App
