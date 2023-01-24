0780edb22ee5216fb67371bfc0a180ec6a4c80cd
juli <julianmenav@gmail.com>
2023-01-24 20:48:37 +0100

    style

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -5,7 +5,7 @@ import projectList from "../../assets/projectList.json";
 export default function Portfolio() {
   return (
     <>
-      <div className="container py-16 px-2 max-w-[430px] sm:max-w-[560px] lg:max-w-[900px] xl:max-w-[1300px] m-auto">
+      <div className="container py-16 px-6 max-w-[430px] sm:max-w-[560px] lg:max-w-[900px] xl:max-w-[1300px] m-auto">
         <p className="text-[50px] font-roboto italic">PROJECTS</p>
         <p className="text-2xl xl:w-2/3 mt-6  font-worksans">
           Some of my projects done during my learning, you can find the rest on
