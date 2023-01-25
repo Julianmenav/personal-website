48d13d479fd337ae262caa7a18041239cf33a223
juli <julianmenav@gmail.com>
2023-01-25 16:06:52 +0100

    style: add button border white color

--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -23,13 +23,13 @@ export default function ProjectCard({ name, picture, description, links }) {
               className="absolute transition-all duration-1000 -right-1/2 flex flex-col gap-6 justify-around items-center grow py-12 "
             >
               <a href={links[0]} target="_blank" className="">
-                <button className="text-black  w-24 py-1 bg-white rounded-full text-center border-2 hover:bg-transparent hover:text-white hover:border-white">
+                <button className="text-black  w-24 py-1 bg-white rounded-full text-center border-2  hover:bg-transparent hover:text-white border-white">
                   <p className="text-[14px] font-worksans">View code</p>
                 </button>
               </a>
               {links[1] ? (
                 <a href={links[1]} target="_blank">
-                  <button className="text-black  w-24 py-1 bg-white rounded-full text-center border-2 hover:bg-transparent hover:text-white hover:border-white">
+                  <button className="text-black  w-24 py-1 bg-white rounded-full text-center border-2  hover:bg-transparent hover:text-white border-white">
                     <p className="text-[14px] font-worksans">Try</p>
                   </button>
                 </a>
